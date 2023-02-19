a5688d972e7d883f5887238cd4e40f356244e824
larrybah <larrybah@gmail.com>
2023-02-19 05:01:20 -0800

    Quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A program that prints on screen using Return statement
+ * Return: 1 (Exit)
+ */
+int main(void)
+{
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
+}
