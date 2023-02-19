900d5cf46d67b40c3ec340f4ee25cc1064d113a8
larrybah <larrybah@gmail.com>
2023-02-19 01:42:52 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
-*	'main'	-	A	program	that	puts	a	sentence	on	screen.
-*	'return':	(0)	exit	program	success.
+* 'main' - A program that puts a sentence on screen.
+* 'return': (0) exit program success.
 */
-int	main(void)
+int main(void)
 {
-	puts("\"Programming	is	like	building	a	multilingual	puzzle");
-	return	(0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
