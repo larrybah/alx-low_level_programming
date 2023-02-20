f7064c4a388cedcf6471b30bf3e1177074b58633
larrybah <larrybah@gmail.com>
2023-02-20 08:18:17 -0800

    combination

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - A program that prints a combination of digits
+ * Return: 0 (Succes)
+ */
+int main(void)
+{
+int = '0';
+while (i <= '9')
+{
+putchar(i);
+if (i != '9')
+{
+putchar(',');
+putchar(' ');
+}
+++i;
+}
+putchar('\n');
+return (0);
+}
