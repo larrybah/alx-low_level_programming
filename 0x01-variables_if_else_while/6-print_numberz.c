fbe3a8eaa6cf20681d0a94f83248f30bb7d1d10a
larrybah <larrybah@gmail.com>
2023-02-20 07:56:46 -0800

    Numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - A progrma that prints numbers
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+int i = '0';
+while (i <= '9')
+{
+putchar(i);
+i++;
+}
+putchar('\n');
+return (0);
+}
