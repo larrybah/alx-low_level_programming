ec736c18692eb5660a6770be0ac70160025c7e16
larrybah <larrybah@gmail.com>
2023-02-20 08:08:21 -0800

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - A program that prints number in hexadecimal
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+int i = '0';
+while (i  <= '9')
+{
+putchar(i);
+i++;
+}
+i = 'a';
+while (i <= 'f')
+{
+putchar(i);
+i++;
+}
+putchar('\n');
+return(0);
+}
