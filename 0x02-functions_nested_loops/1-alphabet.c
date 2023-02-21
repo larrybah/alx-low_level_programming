c0d56e82d87287ddc1128435870a37120abd2ad8
larrybah <larrybah@gmail.com>
2023-02-21 07:50:51 -0800

    alphatbet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,16 +4,13 @@
  * Return: 0 (Success)
  */
 void print_alphabet(void)
-
 {
-	char c = 'a';
-
-	while (c <= 'z')
-	{
-		_putchar(c);
-		c++;
-	}
-	_putchar('\n');
-
-	return (0);
+char c = 'a';
+while (c <= 'z')
+{
+_putchar(c);
+c++;
+}
+_putchar('\n');
+return (0);
 }
