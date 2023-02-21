68e35acfcc0f7c665db61f638998ea71ffd4b096
larrybah <larrybah@gmail.com>
2023-02-21 07:34:42 -0800

    Aphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet - Prints small lettter alphabets
+ * Return: 0 (Success)
+ */
+void print_alphabet(void)
+{
+	char c = 'a';
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+
+	return (0);
+}
