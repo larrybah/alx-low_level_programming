e5e2ab5b0952306ce9f0be6eb1944005da70c098
larrybah <larrybah@gmail.com>
2023-02-22 07:12:43 -0800

    24_hours

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ *
+ *
+ */
+void jack_bauer(void)
+{
+int hours;
+int minutes;
+for (hours = 0; hours < 24; hours++)
+{
+for (minutes = 0; minutes < 60; minutes++)
+{
+_putchar(hours / 10 + '0');
+_putchar(hours % 10 + '0');
+_putchar(':');
+_putchar(minutes / 10 + '0');
+_putchar(minutes % 10 + '0');
+_putchar('\n');
+}
+}
+}
