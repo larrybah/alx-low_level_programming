702c8c6016fd5a9d3d3d877b9a2d7179a200a3da
larrybah <larrybah@gmail.com>
2023-02-22 14:26:10 -0800

    alx low level

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add - add twos numbers
+ * @num1: value 1
+ * @num2: value 2
+ * Return: 0 (sum)
+ */
+int add(int num1, int num2)
+{
+int sum;
+sum = num1 + num2;
+return (sum);
+}
