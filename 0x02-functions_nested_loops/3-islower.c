ea1f6c509a29eb804819bf607d4558421244f8df
larrybah <larrybah@gmail.com>
2023-02-22 04:12:30 -0800

    islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,10 +1,18 @@
 #include "main.h"
 #include <ctype.h>
 /**
- *
+ * main - A program that checks if character is lower
+ * Return: 0 (Success)
  */
 int _islower(int c)
 {
 char = c;
-
+if (islower(c))
+{
+	_putchar('1');
+}
+else 
+{
+	_putchar('0');
+}
 }
