a00de6282a8912287203f7890fc88dce50655da4
larrybah <larrybah@gmail.com>
2023-02-23 08:21:30 -0800

    Debugging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - tests function that prints if integer is positive or negattive
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	i = 98;
+	positive_or_negative(i);
+
+	return (0);
+}
