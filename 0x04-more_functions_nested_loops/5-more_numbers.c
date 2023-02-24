e6150a512626796800ad0709022e02082422f9b8
larrybah <larrybah@gmail.com>
2023-02-24 12:07:48 -0800

    more numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,6 +1,19 @@
 #include "main.h"
 /**
- *
+ * more_numbers - prints numbers from 0 to 14 ten times
  *
  */
+void more_numbers(void)
+{
+	int i, j;
 
+	for (i = 0; i <= 14; i++)
+	{
+		for (j = 0; j <= 10; j++)
+		{
+			_putchar(j);
+		}
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
