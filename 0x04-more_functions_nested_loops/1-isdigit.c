e9da33a3d4da43e711ec0488b284c07b18374c6f
larrybah <larrybah@gmail.com>
2023-02-24 01:59:02 -0800

    isdigit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -6,15 +6,12 @@
  */
 int _isdigit(int c)
 {
-	while (c >= 0 && c <= 9)
-	{
-		if (c)
-		{
-			return (1);
-		}
-		else
-		{
-			return (0);
-		}
-	}
+if (c > 0 && c < 9)
+{
+return (1);
+}
+else
+{
+return (0);
+}
 }
