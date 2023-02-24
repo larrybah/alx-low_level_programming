9c8966c4d4b99cee6da5ef0a259a91d06b0b3001
larrybah <larrybah@gmail.com>
2023-02-24 07:43:12 -0800

    print numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,17 +1,13 @@
 #include "main.h"
 /**
  * print_numbers - prints numbers from 0 to 9
- * Return: 0 (Success)
  */
 void print_numbers(void)
 {
 int i;
-
-while (i >= 48 && i <= 57)
+for (i >= 48; i <= 57; i++)
 {
 _putchar(i);
-i++;
 }
 _putchar('\n');
-return (0);
 }
