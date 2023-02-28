9d70a63aae71ce51bcab5e6954bd79759278e57a
larrybah <larrybah@gmail.com>
2023-02-28 09:30:14 -0800

    puts

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,9 +1,17 @@
 #include "main.h"
 /**
- * puts2 - 
- *
+ * puts2 - prints every other character
+ * @str: parameter that points to a string
+ * Return: void
  */
 void puts2(char *str)
 {
+	int index = 0, len = 0;
 
+	while (str[index++])
+		len++;
+
+	for (index = 0; index < len; index += 2)
+		_putchar(str[index]);
+	_putchar('\n');
 }
