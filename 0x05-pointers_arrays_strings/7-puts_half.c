361f99810d79bd80cfbaa63fcdca6949c9772952
larrybah <larrybah@gmail.com>
2023-02-28 09:36:47 -0800

    puts2

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,9 +1,20 @@
 #include "main.h"
 /**
- *
- *
+ * puts_half - prints half of string
+ * @str: parameter
+ * Return: void
  */
 void puts_half(char *str)
 {
+	int index = 0, len = 0, n;
 
+	while (str[index++])
+		len = len + 1;
+	else
+		n = (len + 1) / 2;
+
+	for (index = n; index < len; index++)
+		_putchar(str[index]);
+
+	_putchar('\n');
 }
