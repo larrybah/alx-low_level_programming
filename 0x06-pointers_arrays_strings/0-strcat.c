2a793f2613bbe8b3f17475e15f55b38f2ebc328a
larrybah <larrybah@gmail.com>
2023-03-02 02:33:41 -0800

    String Concat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,9 +1,23 @@
 #include "main.h"
 /**
- *
- *
+ * *_strcat - concatinate two strings adn prints the result
+ * @dest: Destination
+ * @src: Source
+ * Return: 0 (Success)
  */
 char *_strcat(char *dest, char *src)
 {
+	char *dest[100];
+	char *src[];
+	char strcat[];
 
+	while (strcat == '')
+	{
+		if (*dest == '\0')
+		{
+			strcat = *dest + *src;
+			_putchar(strcat);
+		}
+	}
+	_putchar('\n');
 }
