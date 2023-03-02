f40ae71e6f3170f33b05cdb1738dbb64a9ee979a
larrybah <larrybah@gmail.com>
2023-03-02 05:48:45 -0800

    Reverse

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * reverse_array - reverse an integer
+ * @a: parameter
+ * @n: size
+ * Return: void
+ */
+void reverse_array(int *a, int n)
+{
+	int i, temp;
+
+	for (i = 0; i < n / 2; i++)
+	{
+		temp = *(a + b);
+		*(a + b) = *(a + n - b - 1);
+		*(a + n - b - 1) = temp;
+	}
+	_putchar('\n');
+}
