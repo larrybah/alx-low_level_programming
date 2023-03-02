1e4cf4cfb0c1e1c743858156201cc0c48bc3541a
larrybah <larrybah@gmail.com>
2023-03-02 04:04:55 -0800

    string concat

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,6 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
+
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
@@ -12,6 +14,5 @@ char *leet(char *);
 _putchar(char c);
 char *rot13(char *);
 void print_number(int n);
-
 
 #endif /* MAIN_H */
