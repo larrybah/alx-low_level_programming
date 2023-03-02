47790246337439021a56dade5c91fc4ebe0f35fb
larrybah <larrybah@gmail.com>
2023-03-02 05:52:44 -0800

    Reverse array

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -11,8 +11,8 @@ void reverse_array(int *a, int n)
 
 	for (i = 0; i < n / 2; i++)
 	{
-		temp = *(a + b);
-		*(a + b) = *(a + n - b - 1);
-		*(a + n - b - 1) = temp;
+		temp = *(a + i);
+		*(a + i) = *(a + n - i - 1);
+		*(a + n - i - 1) = temp;
 	}
 }
