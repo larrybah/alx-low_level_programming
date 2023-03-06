72d25f29e5ea9e608448e2ae4bf07e405e9a06fc
larrybah <larrybah@gmail.com>
2023-03-06 08:24:40 -0800

    Set memory

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -8,11 +8,11 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned char *p = s;
+	char *p = s;
 
-	for (unsigned int i = 0; i < n; i++)
+	for (int i = 0; i < n; i++)
 	{
-		s[i] = (unsigned char)b;
+		p[i] = (char)b;
 	}
 	return (s);
 }
