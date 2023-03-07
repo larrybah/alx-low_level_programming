1825061840fd2b4ad6c829cee5ec7bc480f42ea8
larrybah <larrybah@gmail.com>
2023-03-07 08:02:29 -0800

    set string

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * set_string - set the value of a pointerr to char
+ * @s: pointer to pointer to a char
+ * @to: address
+ * Return: void
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
