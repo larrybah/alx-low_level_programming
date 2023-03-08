5aec1854c3cdf25802587bbf11d3cdf46f8462f5
larrybah <larrybah@gmail.com>
2023-03-08 09:09:39 -0800

    puts recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
- * -puts_recursion - prints a string with a trailing new line
+ * _puts_recursion - prints a string with a trailing new line
+ * @s: parameter
  * Return: 0 (Success)
  */
 void _puts_recursion(char *s)
