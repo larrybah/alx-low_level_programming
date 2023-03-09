91e77138424b0decef90916f632cddee6a325936
larrybah <larrybah@gmail.com>
2023-03-09 05:09:16 -0800

    Power

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -7,10 +7,20 @@
  */
 int _pow_recursion(int x, int y)
 {
-	if (x < 0)
+	if (y < 0)
 	{
 		return (-1);
 	}
+	else if (y % 2 == 0)
+	{
+		int temp = _pow_recursion(x, y / 2);
+
+		return (temp * temp);
+	}
 	else
-		return (x + _pow_recursion(x ** y));
+	{
+		int temp = _pow_recursion(x, (y - 1) / 2);
+
+		return (x * temp * temp);
+	}
 }
