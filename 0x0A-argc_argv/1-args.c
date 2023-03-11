aa79f3d6a3b4a8e7a1a54c720f2bb47386e0d3e4
larrybah <larrybah@gmail.com>
2023-03-11 10:42:24 -0800

    args

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -3,6 +3,7 @@
  * main - prints the numbers of arguments passed to a program
  * @argc: parameter that counts the number of arguments passed
  * @argv: parament of array strings passed as command line arguments
+ * Return: 0 (Success)
  */
 int main(int argc, char *argv[])
 {
