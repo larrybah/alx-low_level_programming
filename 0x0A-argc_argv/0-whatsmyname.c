aa726aebcac2e2c57e6d11c5d6698ade4175116b
larrybah <larrybah@gmail.com>
2023-03-11 03:35:10 -0800

    whatsmyname

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * main - prints the name of the file
+ * @argc: parameter that counts the number of arguments pass
+ * @argv: paratmeter of array of strings passed as command line arguments
+ * Return: 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", *argv);
+	return (0);
+}
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]);
+int _putchar(char c);
+
+#endif /* MAIN_H */
