10bddc62f3709b4d1816b0056109c414f9de01f0
larrybah <larrybah@gmail.com>
2023-03-11 09:18:49 -0800

    whatsmyname

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,7 +5,7 @@
  * @argv: paratmeter of array of strings passed as command line arguments
  * Return: 0 (Success)
  */
-int main(int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", *argv);
 	return (0);
