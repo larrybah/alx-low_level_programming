1953a4a4bef49578ce091d372bd755b23583c58e
larrybah <larrybah@gmail.com>
2023-03-13 11:19:56 -0700

    array strings

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * create_array - creates an array of strings
+ * @size: parameter, size of memory
+ * @c: parameter, strings
+ * Return: (Success)
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *s;
+	int i;
+
+	s = malloc((size + 1) * sizeof(char));
+	if (s == NULL)
+	{
+		return (NULL);
+	}
+
+	i = 0;
+
+	while (i < size)
+	{
+		s[i] = c;
+		i++;
+	}
+	s[i] = '\0';
+	return (s);
+}
