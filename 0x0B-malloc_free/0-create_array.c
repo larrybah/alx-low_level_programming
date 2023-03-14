aa471a610f44fafe0595f0f63374c4aaa7770003
larrybah <larrybah@gmail.com>
2023-03-14 04:11:59 -0700

    string array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -8,7 +8,7 @@
 char *create_array(unsigned int size, char c)
 {
 	char *s;
-	int i;
+	unsigned int i;
 
 	s = malloc((size + 1) * sizeof(char));
 
@@ -19,7 +19,7 @@ char *create_array(unsigned int size, char c)
 
 	i = 0;
 
-	while (i > size)
+	while (i < size)
 	{
 		s[i] = c;
 		i++;
