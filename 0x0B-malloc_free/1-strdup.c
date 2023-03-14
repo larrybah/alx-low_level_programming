a9a37a75d8bb099fd27f301e8991fcad2cdfdf2a
larrybah <larrybah@gmail.com>
2023-03-14 06:09:11 -0700

    duplicate

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strdup - prints the duplicate of a string
+ * @str: parameter for duplicate string
+ * Return: Success
+ */
+char *_strdup(char *str)
+{
+	char *s;
+
+	s = malloc(s * sizeof(char));
+
+	if (s == NULL)
+	{
+		return (NULL);
+	}
+	else
+		return (s)
+}
