ad2a93d45a5840f13b7705fc43a1224bfbf1aa45
larrybah <larrybah@gmail.com>
2023-05-18 09:16:16 +0000

    hello world

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - prints text on the screen
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
