19b3d1c6ae7c9b290bd23ce8fbc8d89bd34d8b5e
larrybah <larrybah@gmail.com>
2023-05-18 16:23:59 +0000

    hello world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,8 +5,7 @@
  */
 int main(void)
 {
-	const char *str = "and that piece of art is useful\""
-		" - Dora Korpar, 2015-10-19\n";
+	const char *str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
 	write(STDOUT_FILENO, str, 59);
 	return (1);
