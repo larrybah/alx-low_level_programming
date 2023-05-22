0af4814c7a513541b50ccc42fb9ce3b87428c0a6
larrybah <larrybah@gmail.com>
2023-05-22 15:01:45 +0000

    if else while

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -5,5 +5,21 @@
  */
 int main(void)
 {
-	int i = 0;
+	int i, j, k;
+
+	for (i = 0; i <= 7; i++)
+	{
+		for (j = i + 1; j <= 8; j++)
+		{
+			for (k = j + 1; k <= 9; k++)
+			{
+				putchar(i + '0');
+				putchar(j + '0');
+				putchar(k + '0');
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
 }
