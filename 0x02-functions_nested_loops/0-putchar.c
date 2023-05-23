8431deb91550b251187668b73f772238f94705c0
larrybah <larrybah@gmail.com>
2023-05-23 05:11:48 +0000

    functions nested-loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - prints text to the stdout
+ * Return: 0 on Success.
+ */
+int main(void)
+{
+	char s[] = "_putchar";
+	int i;
+
+	for (i = 0; i <= s[i-1]; i++)
+	{
+		_putchar(s[i]);
+	}
+	return (0);
+}
