fa2cef6f10d45572706efba4f17ecbbc91fba62b
larrybah <larrybah@gmail.com>
2023-05-23 16:57:29 +0000

    functions nested-loops

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints the last digit of a value
+ * @r: value to be return
+ * Return: value
+ */
+int print_last_digit(int i)
+{
+	if (i % 10 == 0)
+	{
+		return (i);
+	}
+	return (i);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * times_table - prints timetable from 0 to 9x
+ */
+void times_table(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			printf("%d, ", (j*i));
+		}
+
+		printf("\n");
+	}
+}
