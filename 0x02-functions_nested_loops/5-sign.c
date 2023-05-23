a22674c7e975fa670de329f60f33588fc57dd46d
larrybah <larrybah@gmail.com>
2023-05-23 09:28:22 +0000

    functions nested-loops

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_sign - prints a plus sign if number is greater and minus sign if its less and zero if its zero.
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+
+	return (0);
+
+}
