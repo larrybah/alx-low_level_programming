fd91dae276a7017bc16a2f51b625ae199589f29c
larrybah <larrybah@gmail.com>
2023-05-24 15:03:58 +0000

    functions nested-loops

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,16 +10,14 @@ void times_table(void)
 	for (i = 0; i <= 9; i++)
 	{
 		for (j = 0; j <= 9; j++)
-			
 		{
-			printf("%d", i * j);
+			printf("%2d", i * j);
 
 			if (!(j == 9))
 			{
 				printf(", ");
 			}
 		}
-
 		printf("\n");
 	}
 }
