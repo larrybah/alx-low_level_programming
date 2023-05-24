d6141105eb600d60dddbe72ee0b865767fa2e461
larrybah <larrybah@gmail.com>
2023-05-24 15:18:18 +0000

    functions nested-loops

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,7 +7,7 @@ void print_to_98(int n)
 {
 	int i = 0;
 
-	if (i >= 98)
+	if (i > 98)
 	{
 		for (i = n; i >= 98; i--)
 		{
@@ -24,7 +24,6 @@ void print_to_98(int n)
 		for (i = n; i <= 98; i++)
 		{
 			printf("%d", i);
-			
 			if (!(i == 98))
 			{
 				printf(", ");
