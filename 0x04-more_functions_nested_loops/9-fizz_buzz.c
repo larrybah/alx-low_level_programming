f3bd709d16c03e996c7a28c234f1a14d230ad874
larrybah <larrybah@gmail.com>
2023-05-25 20:52:00 +0000

    more functions

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * main - prints from 1 - 100
+ * Return: 0 Always
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+			printf("%d ", i);
+	}
+	printf("\n");
+
+	return (0);
+}
