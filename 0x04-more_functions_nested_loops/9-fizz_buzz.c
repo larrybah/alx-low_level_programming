445490469246b34feb6de2ba6fb14311d0a83d27
larrybah <larrybah@gmail.com>
2023-05-25 20:59:10 +0000

    more functions

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,18 +11,18 @@ int main(void)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
 		{
-			printf(" iFizzBuzz ");
+			printf("FizzBuzz ");
 		}
 		else if (i % 3 == 0)
 		{
-			printf(" Fizz ");
+			printf("Fizz ");
 		}
 		else if (i % 5 == 0)
 		{
-			printf(" Buzz ");
+			printf("Buzz ");
 		}
 		else
-			printf(" %d ", i);
+			printf("%d ", i);
 	}
 	printf("\n");
 
