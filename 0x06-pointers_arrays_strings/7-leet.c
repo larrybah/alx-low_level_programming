3a30653fce37d6edd14e36ad8dd46d8d7814231c
larrybah <larrybah@gmail.com>
2023-06-05 04:46:12 +0000

    alx low level

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * leet - print characters in leet format.
+ * Return: leet characters.
+ */
+char *leet(char *str)
+{
+	int i;
+	int *ptr;
+	
+	ptr = str[0];
+
+	for (i = 0; i < str[i - 1]; i++)
+	{
+		
+	}
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -6,6 +6,7 @@
 #include <string.h>
 #include <ctype.h>
 
+char *leet(char *);
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
