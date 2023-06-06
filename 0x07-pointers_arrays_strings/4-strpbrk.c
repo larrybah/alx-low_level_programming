88d6c135678c8e268708f69ad2f3c756803f6903
larrybah <larrybah@gmail.com>
2023-06-06 20:49:18 +0000

    string brsk

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -7,5 +7,20 @@
  */
 char *_strpbrk(char *s, char *accept)
 {
+	while (*s != '\0')
+	{
+		char *ptr;
 
+		ptr = accept;
+		while (*ptr != '\0')
+		{
+			if (*s == *ptr)
+			{
+				return (s);
+			}
+			ptr++;
+		}
+		s++;
+	}
+	return (NULL);
 }
