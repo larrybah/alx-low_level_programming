e7a0c9df2371bffee2977b0b575ec28031e6d6c6
larrybah <larrybah@gmail.com>
2023-06-06 20:36:16 +0000

    decrypt file

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,6 +1,11 @@
 #include "main.h"
 /**
- *
- *
+ * _strpbrk - search for string and finds a matching character
+ * @s: pointer pointin to the string you want to search
+ * @accept: a pointer holding the address to the characters to be check for.
+ * Return: index in found.
  */
 char *_strpbrk(char *s, char *accept)
+{
+
+}
