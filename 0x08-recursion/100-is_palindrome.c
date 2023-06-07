c006ac0d1f6a7ad732f35233d906294b3b11d3aa
larrybah <larrybah@gmail.com>
2023-06-07 20:52:28 +0000

    recursion

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -6,5 +6,17 @@
  */
 int is_palindrome(char *s)
 {
-	if (*s )
+	int len = strlen(s);
+
+	if (len <= 1)
+	{
+		return (1);
+	}
+	else if (s[0] == s[len - 1])
+	{
+		s[len - 1] = '\0';
+		return is_palindrome(s + 1);
+	}
+	else
+		return (0);
 }
