dbb284d3dc0da20d9a1361a01b3e910da4f56647
larrybah <larrybah@gmail.com>
2023-06-07 20:54:10 +0000

    recursion

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -2,7 +2,7 @@
 /**
  * is_palindrome - checks if a word is a palindrome
  * @s: pointer to a string
- * Return: a palindrome 
+ * Return: a palindrome
  */
 int is_palindrome(char *s)
 {
@@ -15,7 +15,7 @@ int is_palindrome(char *s)
 	else if (s[0] == s[len - 1])
 	{
 		s[len - 1] = '\0';
-		return is_palindrome(s + 1);
+		return (is_palindrome(s + 1));
 	}
 	else
 		return (0);
