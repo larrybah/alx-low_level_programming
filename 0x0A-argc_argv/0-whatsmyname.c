2e190a3347ea2c1e6b8ef2198b5ded8cc1dd3209
larrybah <larrybah@gmail.com>
2023-06-09 10:23:05 +0000

    print name, then change itfrom command line

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * main - prints program name using command line args
+ * @argc: number of command line arguments
+ * @argv: command line values
+ * Return: 0 Always
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
