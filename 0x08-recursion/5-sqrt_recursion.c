fee3914c19089f5a115a0586a036f02e8908ae4e
larrybah <larrybah@gmail.com>
2023-06-09 03:59:44 +0000

    gets sqrt using recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -4,14 +4,48 @@
  * @n: parameter, number
  * Return: n, otherwise on error -1
  */
+
+int calculate_sqrt(int n, int x, int y);
+
 int _sqrt_recursion(int n)
 {
-	if (n <= 0)
+	if (n < 1)
 	{
 		return (-1);
 	}
+	else if (n == 0 || n == 1)
+	{
+		return (n);
+	}
 	else
 	{
-		return (n * _sqrt_recursion(n - 1));
+		return (n * calculate_sqrt(n, 1, n));
 	}
+}
+
+/**
+ * calculate_sqrt - calculates square root value
+ * @start: starting point
+ * @end: ending point
+ * @n: number
+ * Return: square root
+ */
+int calculate_sqrt(int n, int x, int y)
+{
+	int i;
+
+	i = (x + y) / 2;
+
+	if (i * i == n)
+	{
+		return (i);
+	}
+	else if (i * i > n)
+	{
+		return (calculate_sqrt(n, x, i - 1));
+	}
+	else
+		return (calculate_sqrt(n, i + 1, y));
+
+	return (calculate_sqrt(n, 1, n));
 }
