91db4463a75435b7527eb5fe9437c815c002f5bb
larrybah <larrybah@gmail.com>
2023-06-15 13:12:45 +0000

    allocate memory for concatenated string

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,43 @@
+#include "main.h"
+/**
+ * string_nconcat - concatenate strings
+ * @s1: strings to be concatenated
+ * @s2: another string to contenated
+ * @n: number of bytes
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int s1_len, s2_len, result_len;
+	char *result;
+
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+
+	s1_len = strlen(s1);
+	s2_len = strlen(s2);
+
+	if (n >= s2_len)
+	{
+		n = s2_len;
+	}
+
+	result_len = s1_len + n;
+
+	result = (char *) malloc((result_len + 1) * sizeof(char));
+
+	if (result == NULL)
+	{
+		return (NULL);
+	}
+
+	strcpy(result, s1);
+	strncat(result, s2, n);
+
+	return (result);
+}
