ed275b9d6a70b12b7febc6bdc999b6ebcc59bcac
larrybah <larrybah@gmail.com>
2023-06-21 15:44:16 +0000

    prints name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,10 @@
+#include "function_pointers.h"
+/**
+ * print_name - prints name using callback function
+ * @name: name to be printed
+ * @f: function pointer that calls name.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
