020c2a1d32ec7fc5fed65c5cc7e045fa708df4d1
larrybah <larrybah@gmail.com>
2023-06-21 19:11:04 +0000

    function pointers

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -14,7 +14,6 @@ int int_index(int *array, int size, int (*cmp)(int))
 	{
 		return (-1);
 	}
-
 	for (i = 0; i < size; i++)
 	{
 		if (cmp(*(array + i)) != 0)
@@ -22,6 +21,5 @@ int int_index(int *array, int size, int (*cmp)(int))
 			return (i);
 		}
 	}
-	
 	return (-1);
 }
