25a69d8a884285a98c7d8c27ea2152db7d929429
larrybah <larrybah@gmail.com>
2023-07-01 14:52:02 +0000

    prints lists elements

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+/**
+ * print_list - prints all the elements in the list
+ * @h: list of elements
+ * Return: elements
+ */
+size_t print_list(const list_t *h)
+{
+	size_t count = 0;
+	int len;
+
+	if (h == NULL)
+	{
+		printf("[0] (nil)\n");
+		return (0);
+	}
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+		{
+			len = 0;
+		}
+		else 
+		{
+			len = strlen(h->str);
+		}
+		printf("[%d] %s\n", len, h->str);
+		h = h->next;
+		count++;
+	}
+
+	return (count);
+}
