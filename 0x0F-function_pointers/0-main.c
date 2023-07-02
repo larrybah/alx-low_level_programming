d332ed2a2d2938c3ea29f821ba10bf4c215c293d
larrybah <larrybah@gmail.com>
2023-07-02 12:40:38 +0000

    prints name

--- 0x0F-function_pointers/0-main.c
+++ 0x0F-function_pointers/0-main.c
@@ -47,6 +47,7 @@ int main(void)
 {
     print_name("Bob", print_name_as_is);
     print_name("Bob Dylan", print_name_uppercase);
+    print_name("Hodor", NULL);
     printf("\n");
     return (0);
 }
