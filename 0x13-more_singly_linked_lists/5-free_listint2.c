8ff91d5b8225e48fb04a31e4580dded11cce3cb9
larrybah <larrybah@gmail.com>
2023-07-04 12:35:19 +0000

    frees list, when head is NULL.

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * free_listint2 - frees lists when head is NULL
+ * @head: head of list
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+	listint_t *next;
+
+	temp = *head;
+	while (temp != NULL)
+	{
+		next = temp->next;
+		free(temp);
+		temp = next;
+	}
+	*head = NULL;
+}
