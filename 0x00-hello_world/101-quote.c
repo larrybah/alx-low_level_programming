e76bd8429de5cdc9f5661b808c52a5c428dccb4b
larrybah <larrybah@gmail.com>
2023-07-21 10:51:40 +0000

    betty: over 80 lines

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,8 +7,7 @@ int main(void)
 {
 	const char *str;
 
-	str = "and that piece of art is useful\""
-		" - Dora Korpar, 2015-10-19\n";
+	str = "and that piece of art is useful""\" - Dora Korpar, 2015-10-19\n";
 
 	write(STDOUT_FILENO, str, 60);
 	return (1);
