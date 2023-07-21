534f415954468e841e3992ad70b6483d7d623769
larrybah <larrybah@gmail.com>
2023-07-21 12:11:48 +0000

    fibonacci up to 50

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -8,14 +8,14 @@ int main(void)
 {
 	unsigned long int num1, num2, fib;
 	unsigned long int i, number;
-	
+
 	number = 50;
 	num1 = 1;
 	num2 = 2;
 
 	printf("%lu, %lu", num1, num2);
 
-	for (i  = 2; i <= number; i++)
+	for (i  = 2; i < number; i++)
 	{
 		fib = num1 + num2;
 
