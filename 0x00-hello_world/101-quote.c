0e60734b65516aca6968ad450d1a0e10a4343ee9
larrybah <larrybah@gmail.com>
2023-07-21 10:02:47 +0000

    betty error: line over 80 characters

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,7 +5,9 @@
  */
 int main(void)
 {
-	const char *str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	const char *str;
+
+	str = "and that piece of art is useful""\" - Dora Korpar, 2015-10-19\n";
 
 	write(STDOUT_FILENO, str, 59);
 	return (1);
