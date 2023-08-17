e260286ad6f8a9cea73ae10e59b4b6dd87f8000c
larrybah <larrybah@gmail.com>
2023-08-17 10:02:33 +0000

    prints elements

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -8,8 +8,8 @@ size_t print_dlistint(const dlistint_t *h)
 {
 	const dlistint_t *current = h;
 	size_t len = 0;
-	
-	while(current != NULL)
+
+	while (current != NULL)
 	{
 		printf("%d\n", current->n);
 		current = current->next;
