5c8ac419d8feceeda10261242c963794454e9f57
larrybah <larrybah@gmail.com>
2023-08-17 11:31:09 +0000

    frees the list

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+ * free_dlistint - frees the double linked list
+ * @head: head of list
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *current = head;
+
+	while (current != NULL)
+	{
+		dlistint_t *temp = current;
+		current = current->next;
+		free(temp);
+	}
+}
