97b402bd3d07b842230e2bdce524d3bd3edbb2c5
larrybah <larrybah@gmail.com>
2023-11-29 15:31:50 +0000

    new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -9,16 +9,36 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_dog_ptr;
+	int len1, len2;
 
-	new_dog_ptr = (dog_t *)malloc(sizeof(dog_t));
+	len1 = strlen(name);
+	len2 = strlen(owner);
+
+	new_dog_ptr = malloc(sizeof(dog_t));
 
 	if (new_dog_ptr == NULL)
 	{
 		return (NULL);
 	}
 
-	new_dog_ptr->name = name;
-	new_dog_ptr->owner = owner;
+	new_dog_ptr->name = malloc(sizeof(char) * (len1 + 1));
+
+	if (new_dog_ptr->name == NULL)
+	{
+		free(new_dog_ptr);
+		return (NULL);
+	}
+
+	new_dog_ptr->owner = malloc(sizeof(char) * (len2 + 1));
+	if (new_dog_ptr->owner == NULL)
+	{
+		free(new_dog_ptr);
+		free(new_dog_ptr->name);
+		return (NULL);
+	}
+
+	strcpy(new_dog_ptr->name, name);
+	strcpy(new_dog_ptr->owner, owner);
 	new_dog_ptr->age = age;
 
 	return (new_dog_ptr);
