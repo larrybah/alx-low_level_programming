e38757964383fd084fb28da1dfb23d12b68c702e
larrybah <larrybah@gmail.com>
2023-11-29 15:12:23 +0000

    Bob - poppy

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -6,13 +6,12 @@
 void print_dog(struct dog *d)
 {
 	if (d == NULL)
-	{
 		return;
-	}
-	else
-	{
-		printf("Name: %s\n", (d->name != NULL) ? d->name : ("nil"));
-		printf("Age: %f\n", d->age);
-		printf("Owner: %s\n", (d->owner != NULL) ? d->owner : ("nil"));
-	}
+
+	if (d->name == NULL)
+		d->name = "(nil)";
+	if (d->owner == NULL)
+		d->owner = "(nil)";
+
+	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }
