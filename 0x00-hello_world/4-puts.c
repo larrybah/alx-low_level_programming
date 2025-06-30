9ed177e4032e531727eb8299ed136083f9e56ef2
larrybah <larrybah@gmail.com>
2025-06-30 22:41:21 +0000

    prints string using puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
-
 /**
  * main - Entry point
  *
- * Return 0
+ * Return: 0 on success
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle\n");
-	return (0);	
+	return (0);
 }
